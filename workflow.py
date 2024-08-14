83776f9d89b3ba3899c52401b9fe5f23c70f3ebc
Philip Hackstock <20710924+phackstock@users.noreply.github.com>
2024-08-14 12:00:13 +0200

    Add public workflow function

--- workflow.py
+++ workflow.py
@@ -1,15 +1,18 @@
 from pathlib import Path
-import pyam
+from pyam import IamDataFrame
 from nomenclature import DataStructureDefinition, RegionProcessor, process
 
 
 here = Path(__file__).absolute().parent
 
 
-def main(df: pyam.IamDataFrame) -> pyam.IamDataFrame:
+def main(df: IamDataFrame) -> IamDataFrame:
     """Project/instance-specific workflow for scenario processing"""
 
     # Run the validation and region-processing
     dsd = DataStructureDefinition(here / "definitions")
     processor = RegionProcessor.from_directory(path=here / "mappings", dsd=dsd)
     return process(df, dsd, processor=processor)
+
+def public(df: IamDataFrame) -> IamDataFrame:
+    return df
