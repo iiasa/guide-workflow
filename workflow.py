2dca73673b08b69f2b9cdc660c41cff6b16c78a9
Daniel Huppmann <dh@dergelbesalon.at>
2024-11-04 16:35:49 +0100

    Add snakemake processing to workflow.py

--- workflow.py
+++ workflow.py
@@ -1,9 +1,23 @@
 from pathlib import Path
 from pyam import IamDataFrame
 from nomenclature import DataStructureDefinition, RegionProcessor, process
+import logging
 
 
 here = Path(__file__).absolute().parent
+
+
+logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler(snakemake.log[0], mode="a")
+file_handler.setLevel(logging.DEBUG)
+
+# Define a logging format (optional)
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", "%Y-%m-%d %H:%M:%S")
+file_handler.setFormatter(formatter)
+
+for logger_name in ["ixmp4", "pyam", "nomenclature", logger.name]:
+    logging.getLogger(logger_name).addHandler(file_handler)
 
 
 def main(df: IamDataFrame) -> IamDataFrame:
@@ -14,5 +28,6 @@ def main(df: IamDataFrame) -> IamDataFrame:
     processor = RegionProcessor.from_directory(path=here / "mappings", dsd=dsd)
     return process(df, dsd, processor=processor)
 
-def public(df: IamDataFrame) -> IamDataFrame:
-    return df
+logger.info(f"Starting processing for {snakemake.input[0]}")
+main(pyam.IamDataFrame(snakemake.input[0])).to_excel(snakemake.output[0])
+logger.info(f"Successfully finished processing for {snakemake.input[0]}")
